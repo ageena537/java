5d8f7d4625ea6c83060a41c61ab6564a0f264ed9
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2025-08-13 10:30:44 +0530

    Add files via upload

---
+++ Matrixatranspose.java
@@ -0,0 +1,34 @@
+package ageena;
+
+import java.util.Scanner;
+
+public class Matrixadd {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no. of rows:");
+		int rows=sc.nextInt();
+		System.out.println("Enter the no. of cols:");
+		int cols=sc.nextInt();
+		int[][]matrix=new int[rows][cols];
+		int[][]transpose=new int[cols][rows];
+		System.out.println("Enter the matrix elements:");
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				matrix[i][j]=sc.nextInt();
+				
+			}
+		}
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				transpose[j][i]=matrix[i][j];
+				
+			}
+		}
+		 System.out.println("Transpose Matrix:");
+	        for (int i = 0; i < cols; i++) {
+	            for (int j = 0; j < rows; j++) {
+	                System.out.print(transpose[i][j] + " ");
+	            }
+	            System.out.println();
+	}}
+}
