62ae3f99474d3a882dd767ccff2dd145722cfb8d
ageena537 <ageenamathew2028@cs.sjcetpalai.ac.in>
2025-08-13 10:00:07 +0530

    Add files via upload

---
+++ Multiarray.java
@@ -0,0 +1,18 @@
+package ageena;
+
+public class Multiarray {
+public static void main(String[] args) {
+	int [][]matrix= {{1,2,3,4},
+			{3,5,6,7}};
+	System.out.println(matrix.length);
+	System.out.println(matrix[0].length);
+	System.out.println(matrix[1].length);
+	for(int i=0;i<matrix.length;i++){
+		for(int j=0;j<matrix[i].length;j++) {
+			System.out.print(matrix[i][j]+"\t");
+			
+		}System.out.println();
+	}
+	}
+}
+
